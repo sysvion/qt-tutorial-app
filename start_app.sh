7f2173bfb7b8e7e6ecfd87972ac392af20cd4e89
Aron Dingemanse <aron@dingweb.nl>
2026-06-22 12:17:48 +0200

    make paths relative

--- start_app.sh
+++ start_app.sh
@@ -1,8 +1,14 @@
 #! /usr/bin/env bash
 
+PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+
 echo Debug output for ros tutorial gui
+
 . /opt/ros/jazzy/setup.bash
-. ~/gui_ws/install/setup.bash
-. ~/gui_ws/venv/bin/activate
-export PYTHONPATH="/home/practicum/gui_ws/venv/lib/python3.12/site-packages:$PYTHONPATH"
+. $PROJECT_ROOT/../install/install/setup.bash
+. $PROJECT_ROOT/venv/bin/activate
+
+export PYTHONPATH="$PROJECT_ROOT/venv/lib/python3.12/site-packages:$PYTHONPATH"
+
+# if the app crash give ample time to read the error msg
 ros2 run qt main || sleep 10s
